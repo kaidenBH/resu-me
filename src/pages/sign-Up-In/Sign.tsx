163b907803a379317df42fced8ba40f903f5548f
yasuoPi <haitem.bourahla7@gmail.com>
2023-10-09 21:55:05 +0100

    fixed the animation settup

--- src/pages/sign-Up-In/Sign.tsx
+++ src/pages/sign-Up-In/Sign.tsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { Box } from '@mui/material';
 import { useAuth } from '../../components/context/AuthContext';
 import SignUp, { SignUpFormData } from "./SignUp";
 import SignIn, { SignInFormData } from "./SignIn";
 import Slider from "./Slider";
-import './sliderEffect.css';
 
 const Sign = () => {
   const { signIn, signUp } = useAuth();
@@ -23,27 +21,18 @@ const Sign = () => {
     console.log("Button clicked", haveAccount);
   };
   return (
-    <Box sx={{ display: 'flex', padding: 0 }}>
-      <TransitionGroup>
-        {haveAccount ? (
-          <CSSTransition
-            key="slider-signin"
-            timeout={500}
-            classNames="slide"
-          >
-            <Slider onSubmit={() => SlideEffect()} haveAccount={haveAccount} />
-          </CSSTransition>
-        ) : (
-          <CSSTransition
-            key="sign-up"
-            timeout={500}
-            classNames="slide"
-          >
-            <SignUp onSubmit={SignUpFormData} />
-          </CSSTransition>
-        )}
-      </TransitionGroup>
-      <Slider onSubmit={() => SlideEffect()} haveAccount={haveAccount} />
+    <Box sx={{ display: 'flex', padding: 0 }} >
+      {haveAccount ? (
+        <>
+          <Slider onSubmit={() => SlideEffect()} haveAccount={haveAccount} />
+          <SignIn onSubmit={SignInFormData} />
+        </>
+      ) : (
+        <>
+          <SignUp onSubmit={SignUpFormData} />
+          <Slider onSubmit={() => SlideEffect()} haveAccount={haveAccount} />
+        </>
+      )}
     </Box>
   );
 };
